b7d6cbcd510f9675a5cf067c14aacf81659d7402
Zyd <zhangyaodong417@163.com>
2017-09-14 11:45:20 +0800

    代码格式化

--- hot-reload.js
+++ hot-reload.js
@@ -1,53 +1,31 @@
 'use strict'
-
 const path = require('path');
-
 const cp = require('child_process');
-
 const chokidar = require('chokidar');
-
 const watcher = chokidar.watch(path.join(__dirname, '/server'));
-
 let appIns = cp.fork(path.join(__dirname, '/server/pack.js'));
 
 watcher.on('ready', () => {
+    watcher.on('change', (path) => {
+        console.log(path + ' change');
+        appIns = reload(appIns);
+    });
 
-        watcher.on('change', (path) => {
+    watcher.on('add', (path) => {
+        console.log(path + ' add');
+        appIns = reload(appIns);
+    });
 
-                console.log('<---- watched file change, do something ---->');
-
-                appIns = reload(appIns);
-
-        });
-
-        watcher.on('add', (path) => {
-
-                console.log('<---- watched new file add, do something ---->');
-
-                appIns = reload(appIns);
-
-        });
-
-        watcher.on('unlink', (path) => {
-
-                console.log('<---- watched file remove, do something ---->');
-
-                appIns = reload(appIns);
-
-        });
-
+    watcher.on('unlink', (path) => {
+        console.log(path + ' remove');
+        appIns = reload(appIns);
+    });
 });
-
 process.on('SIGINT', () => {
-
-        process.exit(0);
-
+    process.exit(0);
 });
 
 function reload(appIns) {
-
-        appIns.kill('SIGINT');
-
-        return cp.fork(require('path').join(__dirname, '/server/pack.js'));
-
+    appIns.kill('SIGINT');
+    return cp.fork(require('path').join(__dirname, '/server/pack.js'));
 }
